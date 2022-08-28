6aaf468677e0c3dfb95b4556b8b57485fedb637e
carlos <clsantiago@student.fullsail.edu>
2022-08-28 17:23:52 -0400

    fixed validation

--- src/main/java/io/thedatapirates/financeapi/domains/investments/InvestmentDTO.java
+++ src/main/java/io/thedatapirates/financeapi/domains/investments/InvestmentDTO.java
@@ -22,10 +22,8 @@ public class InvestmentDTO extends BaseEntityDTO {
     @Size(min = 3, message = StringConstants.SECTOR_MIN)
     private String sector;
 
-    @NotBlank(message = StringConstants.AMOUNT_REQUIRED)
     private double amount;
 
-    @NotBlank(message = StringConstants.BUY_PRICE_REQUIRED)
     private double buyPrice;
 
     public InvestmentDTO() {
